b8eb80bc389aaf914fd9d73683dedadb3a6e1ca9
Andres Delgado <adelgado0723@gmail.com>
2022-06-29 16:56:49 -0400

    added some patches and color theming

--- config.h
+++ config.h
@@ -1,12 +1,31 @@
 /* user and group to drop privileges to */
-static const char *user  = "adelg";
+static const char *user = "adelg";
 static const char *group = "adelg";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
+    [INIT] = "#282a36",   /* after initialization */
+    [INPUT] = "#bd93f9",  /* during input */
+    [FAILED] = "#ff5555", /* wrong password */
+	[CAPS] = "#ffb86c",         /* CapsLock on */
 };
 
 /* treat a cleared input like a wrong password (color) */
+static const char *imgpath = "./images/dracula.xpm";
+static const int imgwidth = 512;
+static const int imgheight = 512;
+static const int imgoffsetx = 704;
+static const int imgoffsety = 284;
+static const int showimgonlyatstart = 1;
 static const int failonclear = 1;
+
+
+// static const char *imgpath = "./images/arch.xpm";
+// static const int imgwidth = 1920;
+// static const int imgheight = 1080;
+// static const int imgoffsetx = 0;
+// static const int imgoffsety = 0;
+// static const int showimgonlyatstart = 1;
+// static const int failonclear = 1;
+//
+/* time in seconds before the monitor shuts down */
+static const int monitortime = 5;
